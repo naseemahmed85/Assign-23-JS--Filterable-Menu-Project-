bfb8c49666267a960a6d9419edb53248ca3e48a6
naseemahmed85 <naseem.ahmedabbasi786@gmail.com>
2025-11-05 00:35:02 +0500

    Assign-23-JS Filterable Menu Project

---
+++ script.js
@@ -0,0 +1,336 @@
+
+const resturanMenu = [
+    {
+        title: 'Chicken Biryani',
+        price: 300,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/chicken-biryani.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Chicken Pulao',
+        price: 350,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/chicken-pulao.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Mutton Biryani',
+        price: 400,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/mutton-biryani.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Mutton Pulao',
+        price: 450,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/mutton-pulao.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Beef Biryani',
+        price: 450,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/beef-biryani.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Beef Pulao',
+        price: 500,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/beef-pulao.jpg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Aloo Paratha',
+        price: 150,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/aloo-paratha.jpeg',
+        category: 'Breakfast'
+    },
+
+    {
+        title: 'Anda Paratha',
+        price: 200,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/anda-paratha.jpeg',
+        category: 'Breakfast'
+    },
+
+    {
+        title: 'Apple Shake',
+        price: 300,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/apple-shake.jpg',
+        category: 'Shakes'
+    },
+
+    {
+        title: 'Banana Shake',
+        price: 350,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/banana-shake.jpg',
+        category: 'Shakes'
+    },
+
+    {
+        title: 'Beef Korma',
+        price: 550,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/beef-corma.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Bheja Fry',
+        price: 1000,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/bheja-fry.jpg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Bread Omelette',
+        price: 200,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/bread-omelette.jpg',
+        category: 'Breakfast'
+    },
+
+    {
+        title: 'Chicken Korma',
+        price: 320,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/chicken-korma.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Coca Cola Drink',
+        price: 250,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/coca-cola-drink.jpg',
+        category: 'Colddrink'
+    },
+
+    {
+        title: 'Custard',
+        price: 550,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/custard.jpg',
+        category: 'Sweetdish'
+    },
+
+    {
+        title: 'Doodh Dulari',
+        price: 650,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/doodh-dulari.jpeg',
+        category: 'Sweetdish'
+    },
+
+    {
+        title: 'Gajar Ka Halwa',
+        price: 750,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/gajar-ka-halwa.jpg',
+        category: 'Sweetdish'
+    },
+
+    {
+        title: 'Halwa Puri',
+        price: 320,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/halwa-puri.jpeg',
+        category: 'Breakfast'
+    },
+
+    {
+        title: 'Ice Cream',
+        price: 520,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/ice-cream.webp',
+        category: 'Icecream'
+    },
+
+    {
+        title: 'Milk Shake',
+        price: 400,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/milk-shake.jpg',
+        category: 'Shakes'
+    },
+
+    {
+        title: 'Mutton Corma',
+        price: 900,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/mutton-corma.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Mutton Paya',
+        price: 950,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/mutton-paya.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Nihari',
+        price: 650,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/nihari.jpeg',
+        category: 'Lunch'
+    },
+
+    {
+        title: 'Pepsi Drink',
+        price: 250,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/pepsi-drink.webp',
+        category: 'Colddrink'
+    },
+
+    {
+        title: 'Sawaiyan',
+        price: 350,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/sawaiyan.jpeg',
+        category: 'Sweetdish'
+    },
+
+    {
+        title: 'Seven Up Drink',
+        price: 250,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/sevenup-drink.jpg',
+        category: 'Colddrink'
+    },
+
+    {
+        title: 'Sprite Drink',
+        price: 250,
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quam asperiores incidunt maxime qui, voluptates illo fugiat culpa voluptatibus debitis iusto laborum aperiam numquam ipsam consequuntur velit veniam aspernatur!',
+        imgUrl: './assets/sprite-drink.webp',
+        category: 'Colddrink'
+    },
+
+];
+
+const btnArr = [
+    'All',
+    'BreakFast',
+    'Lunch',
+    'Shakes',
+    'ColdDrink',
+    'SweetDish',
+    'IceCream',
+];
+
+const myBtnArryForBody = btnArr.map(function (btn) {
+    return `<button onclick="menu('${btn}')">${btn}</button>`
+});
+
+document.querySelector('#btn').innerHTML = myBtnArryForBody.join('')
+
+
+menu()
+
+    function menu(category) {
+        // It will return always Array
+        // It will give you filtered length according to your condition
+        // Will only return if the condition is true
+
+        const myMenuHTMLForBody = resturanMenu.filter(function (menu) {
+
+            if (category && category.toLowerCase() !==  'all') {
+                
+                return menu.category.toLowerCase() == category.toLowerCase()
+            } else {
+                return true
+            }
+
+        }).map(function (menu) {
+                    const myMenuHTML = `<div class="foodItem">
+                                <!-- image -->
+                                <div class="imgDiv">
+                                    <img src="${menu.imgUrl}" alt="chicken-biryani">
+                                </div>
+                                <!-- Title and Price -->
+                                <div class="titlDescription">
+                                    <div class="titlePrice">
+                                        <h2>${menu.title}</h2>
+                                        <p>${menu.price}</p>
+                    
+                                    </div>
+                                    <!-- Description -->
+                                    <p>${menu.description}</p>
+                                </div>
+                            </div>`
+        
+                            return myMenuHTML
+                    
+                });
+        
+            document.querySelector('#foodItems').innerHTML = myMenuHTMLForBody.join('')
+    }
+
+const searchBox = document.getElementById('searchBox');
+const searchForm = document.getElementById('searchForm');
+const foodItemsDiv = document.getElementById('foodItems');
+
+searchForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // form reload na kare
+
+    const value = searchBox.value.toLowerCase();
+
+  // agar kuch likha nahi gaya to sab items wapas dikha do
+    if (value === '') {
+        menu('All');
+        return;
+    }
+
+  // filter karo matching title ke hisaab se
+    const filtered = resturanMenu.filter(function (item) {
+        return item.title.toLowerCase().startsWith(value);
+    });
+
+  // agar koi match nahi mila
+    if (filtered.length === 0) {
+        foodItemsDiv.innerHTML = `<p style="text-align:center; font-Size: 25px; display: flex; color:#c59d5f;">No matching items found!</p>`;
+        return;
+    }
+
+  // matched items display karo
+    const resultHTML = filtered.map(function (menu) {
+    return `
+        <div class="foodItem">
+        <div class="imgDiv">
+            <img src="${menu.imgUrl}" alt="${menu.title}">
+        </div>
+        <div class="titlDescription">
+            <div class="titlePrice">
+            <h2>${menu.title}</h2>
+            <p>${menu.price}</p>
+        </div>
+            <p>${menu.description}</p>
+        </div>
+        </div>
+    `;
+    });
+
+    foodItemsDiv.innerHTML = resultHTML.join('');
+});
+
+
+
+
